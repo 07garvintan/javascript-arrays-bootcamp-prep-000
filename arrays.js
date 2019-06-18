d5af2049b50e8fa6444e51a6e9c923e525495bd2
garvin tan <garvintan07@gmail.com>
2019-06-18 19:34:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,17 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array, n) {
-  
+  array.unshift(n)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, n) {
   
 }
 
+function addElemenToEndOfArray(array,n) {
+  
+}
+
+function destructivelyAddElementToEndOfArray(array,n) {
+  
+}
