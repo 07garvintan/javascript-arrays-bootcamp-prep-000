f116d087c6911fc971d3852507bc1e422f0d3b22
garvin tan <garvintan07@gmail.com>
2019-06-18 20:11:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,6 +41,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  newArray= array.slice(-2)
+  newArray= array.slice(0, array.length-1)
   return newArray
 }
