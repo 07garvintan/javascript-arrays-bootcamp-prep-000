4b4a97af3c88680b8b9e974bf742ee69b5fc8d45
garvin tan <garvintan07@gmail.com>
2019-06-18 19:43:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array, element) {
   newArray = [element,...array]
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
